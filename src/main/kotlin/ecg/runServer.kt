f607c067b2df61667ebd9d3fbd16a818eaf0a64a
Drew Siedel <drew@beaglebone.localdomain>
2019-07-21 15:11:38 +0000

    Add ability to serve static files (html, css, js)

--- src/main/kotlin/ecg/runServer.kt
+++ src/main/kotlin/ecg/runServer.kt
@@ -1,18 +1,41 @@
 package ecg
 
-import io.ktor.server.netty.*
-import io.ktor.routing.*
-import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.http.cio.websocket.CloseReason
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.content.*
+import io.ktor.routing.*
+import io.ktor.util.*
+import java.time.*
+
+fun Application.landingModule() {
+    routing {
+        get("/") {
+	    call.respondRedirect("/index.html", permanent = true)
+        }
+    }
+}
+
+fun Application.staticModule() {
+    routing {
+        static {
+            files("resources/html")
+	    files("resources/css")
+	    files("resources/js")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, 8888) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
-        }
-    }.start(wait = true)
+    val server = embeddedServer(Netty, 8888) {
+	landingModule()
+	staticModule()
+    }
+    server.start(wait = true)
 }
