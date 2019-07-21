15771a8a16429890a21d0f9817c6f878a8d4768c
Drew Siedel <drew@beaglebone.localdomain>
2019-07-21 15:50:40 +0000

    Add basic websocket support to the server
    It is just running basic echo functionality, and I plan
    to add the protocol soon.

--- README.md
+++ README.md
@@ -4,4 +4,18 @@
 ./gradlew
 ```
 
-Then you can see the page at `localhost:8888`
+Then you can see the page at `localhost:8888` (it will redirect to the landing
+page). Then you can open a websocket by typing in the web console:
+
+```
+var ws = new WebSocket();
+ws.onmessage = msg => console.log(msg);
+```
+
+Send messages with:
+
+```
+ws.send("some text");
+```
+
+and you should see an echoed response from the server.
--- src/main/kotlin/ecg/runServer.kt
+++ src/main/kotlin/ecg/runServer.kt
@@ -1,18 +1,24 @@
 package ecg
 
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.application.*
 import io.ktor.features.*
-import io.ktor.http.cio.websocket.*
-import io.ktor.http.cio.websocket.CloseReason
-import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.*
 import io.ktor.http.content.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.ktor.util.*
-import java.time.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.*
+
+//TODO: make a protocol for adding websocket clients to this list
+//TODO: don't forget there will be two different types of websockets connecting,
+//      the Python one (which should be registered as a source), and the browser
+//      one (which should be registered as a sink). Use the protocol to distinguish
+val connectedWebsockets = listOf<String>()
 
 fun Application.landingModule() {
     routing {
@@ -32,10 +38,28 @@ fun Application.staticModule() {
     }
 }
 
+fun Application.websocketModule() {
+    install(WebSockets)
+    routing {
+        webSocket("/ws") {
+            try {
+                while (true) {
+                    val text = (incoming.receive() as Frame.Text).readText()
+                    println("Received ${text}")
+		    outgoing.send(Frame.Text("You sent ${text}!"))
+                }
+            } catch (e: ClosedReceiveChannelException) {
+               println("Socket connection closed.")
+            }
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8888) {
 	landingModule()
 	staticModule()
+        websocketModule()
     }
     server.start(wait = true)
 }
